4c8064ec6fc19ba144f5ac21b33ed6fbf0ff94d9
naruse666 <0x706f6b6f@gmail.com>
2026-04-26 12:15:36 +0900

    let

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,23 +3,28 @@
 require 'rails_helper'
 
 RSpec.describe User do
+  let(:name) { 'Alice' }
+  let(:age) { 20 }
+  let(:user) { User.new(name: name, age: age) }
+
   describe '#greet' do
     it 'Hello + 名前の文字列を返す' do
-      user = User.new(name: 'Alice', age: 20)
       expect(user.greet).to eq 'Hello, Alice!'
     end
   end
 
   describe '#adult?' do
     context '18歳以上の場合' do
+      let(:age) { 18 }
+
       it 'trueを返す' do
-        user = User.new(name: 'Alice', age: 18)
         expect(user.adult?).to be true
       end
     end
     context '17歳以下の場合' do
+      let(:age) { 17 }
+
       it 'falseを返す' do
-        user = User.new(name: 'Alice', age: 17)
         expect(user.adult?).to be false
       end
     end
