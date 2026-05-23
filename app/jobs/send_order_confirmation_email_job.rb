0c68d7d7f9695d54a7f3b5faccc826a8ab028bd8
naruse666 <0x706f6b6f@gmail.com>
2026-05-23 19:38:54 +0900

    job retry strategy

--- app/jobs/send_order_confirmation_email_job.rb
+++ app/jobs/send_order_confirmation_email_job.rb
@@ -1,7 +1,14 @@
 class SendOrderConfirmationEmailJob < ApplicationJob
+  class JobError < StandardError; end
   queue_as :default
 
+  discard_on ActiveRecord::RecordNotFound
+  retry_on JobError, wait: :polynomially_longer, attempts: 3
+
   def perform(order_id)
+    # For test
+    # raise JobError, 'TEST JOB ERROR'
+
     order = Order.find(order_id)
     return if order.confirmation_email_sent_at.present?
 
